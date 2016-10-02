034f5c58b23ef40ccc60208579f870b890713f17
Walker Hannan <awahannan@gmail.com>
2016-10-02 12:14:13 -0400

    Abstracted input/output from eightball class

--- magic8ball/api/api.py
+++ magic8ball/api/api.py
@@ -1,19 +1,22 @@
-import random
-
 from flask import Flask
+from flask import request
+
+from magic8ball.app.eightball import EightBall
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "index"
 
+
 @app.route("/answer")
 def answer():
-    list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-    rand = random.Random()
-    index = rand.randint(0, len(list) - 1)
-    return list[index]
+    question = request.args.get('question')
+    eightball = EightBall()
+    return eightball.ask_question(question)
+
 
 if __name__ == "__main__":
     app.run()
--- magic8ball/app/eightball.py
+++ magic8ball/app/eightball.py
@@ -1,23 +1,26 @@
 import random
 
+
 class EightBall:
-	yesNoResponses = ["No", "yes", "Maybe", "idk go ask you mother "]
-	whenResponses = ["Now", "yesterday", "In exactly 12 minutes", "tomorrow", "never"]
-	allResponses = yesNoResponses + whenResponses
+    yesNoResponses = ["No", "yes", "Maybe", "idk go ask you mother "]
+    whenResponses = ["Now", "yesterday", "In exactly 12 minutes", "tomorrow", "never"]
+    allResponses = yesNoResponses + whenResponses
+
+    def ask_question(self, input_question):
+        question = input_question.lower().split(" ")
+        first_word = question[0]
+        if first_word == "should" or first_word == "can" or first_word == "will" or first_word == "what":
+            return random.choice(self.yesNoResponses)
+
+        elif first_word == "when":
+            return random.choice(self.whenResponses)
+        else:
+            return random.choice(self.allResponses)
 
 
-	def ask_question(self):
-		question = input("Ask me a question ").lower().split(" ")
-		first_word = question[0]
-		if first_word == "should" or first_word == "can" or first_word == "will" or first_word == "what":
-			print(random.choice(self.yesNoResponses))
-	
-		elif first_word == "when":
-			print(random.choice(self.whenResponses))
-		else:
-			print(random.choice(self.allResponses))
-	
-		
 if __name__ == "__main__":
-	eightball = EightBall()
-	eightball.ask_question()
+    question = input("Ask me a question ")
+
+    eightball = EightBall()
+    answer = eightball.ask_question(question)
+    print(answer)
